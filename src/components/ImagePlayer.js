31cfc3149c6b3279ac2bae44cffbc29976cdb7b2
eric <eric355402@yahoo.com>
2020-06-30 22:17:24 -0600

    added react carousel

--- src/components/ImagePlayer.js
+++ src/components/ImagePlayer.js
@@ -1,11 +1,33 @@
 import React from "react";
-import { Container, Image } from "semantic-ui-react";
+import { Container, Image, Divider } from "semantic-ui-react";
+import { CarouselProvider, Slider, Slide } from "pure-react-carousel";
+import "pure-react-carousel/dist/react-carousel.es.css";
+
+import CustomDotGroup from "./CustomDotGroup";
 
 const ImagePlayer = () => {
   return (
-    <Container>
-      <Image src="/images/dock.jpg" alt="dock" fluid centered />
-      <a href="https://www.pexels.com">Photos provided by Pexels</a>
+    <Container style={{ marginTop: "75px" }}>
+      <CarouselProvider
+        naturalSlideWidth={100}
+        naturalSlideHeight={60}
+        totalSlides={3}
+      >
+        <Slider>
+          <Slide index={0}>
+            <Image src="/images/dock.jpg" alt="dock" fluid centered />
+            <a href="https://www.pexels.com">Photos provided by Pexels</a>
+          </Slide>
+          <Slide index={1}>
+            <Image src="/images/woman.jpg" alt="dock" fluid centered />
+          </Slide>
+          <Slide index={2}>
+            <Image src="/images/close-up.jpg" alt="dock" fluid centered />
+          </Slide>
+        </Slider>
+        <Divider />
+        <CustomDotGroup slides={3} />
+      </CarouselProvider>
     </Container>
   );
 };
